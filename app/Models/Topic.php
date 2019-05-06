e7c761e0bab2508acdd8a451a5aed7f4a0cde9d6
vagrant <vagrant@homestead>
2019-05-06 15:30:24 +0000

    话题排序

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -19,4 +19,46 @@ class Topic extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    /**
+     * 本地作用域 使用scope开头，调用时 withOrder()即可
+     * @param $query [查询构造器(默认传参)]
+     * @param $order [排序方式]
+     */
+    public function scopeWithOrder($query, $order)
+    {
+        switch ($order){
+            case 'recent':
+                $query->recent();
+                break;
+            default:
+                $query->recentReplied();
+                break;
+        }
+        //预防 N+1 问题
+        $query->with('user','category');
+    }
+
+    /**
+     * 更新时间倒序
+     * @param $query
+     * @return mixed
+     */
+    public function scopeRecent($query)
+    {
+        // 当话题有新回复时，我们将编写逻辑来更新话题模型的 reply_count 属性，
+        // 此时会自动触发框架对数据模型 updated_at 时间戳的更新
+        return $query->orderBy('updated_at','desc');
+    }
+
+    /**
+     * 创建时间倒序
+     * @param $query
+     * @return mixed
+     */
+    public function scopeRecentReplied($query)
+    {
+        //按照创建时间排序
+        return $query->orderBy('created_at','desc');
+    }
 }
