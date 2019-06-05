fba71daf42c2b7e72238a723dbc3862eb4a659a8
vagrant <vagrant@homestead>
2019-06-05 15:14:54 +0000

    用户可以创建话题

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Topic;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
-
+use Auth;
 class TopicsController extends Controller
 {
     public function __construct()
@@ -14,6 +15,12 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
+    /**
+     * 话题首页
+     * @param Request $request
+     * @param Topic $topic
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
 	public function index(Request $request, Topic $topic)
 	{
         $topics = $topic->withOrder($request->order)->paginate(20);
@@ -25,23 +32,50 @@ class TopicsController extends Controller
         return view('topics.show', compact('topic'));
     }
 
+    /**
+     * 显示话题创建页
+     * @param Topic $topic
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
 	public function create(Topic $topic)
 	{
-		return view('topics.create_and_edit', compact('topic'));
+	    $categories = Category::all();
+		return view('topics.create_and_edit', compact('topic','categories'));
 	}
 
-	public function store(TopicRequest $request)
+    /**
+     * 创建话题
+     * @param TopicRequest $request
+     * @param Topic $topic
+     * @return \Illuminate\Http\RedirectResponse
+     */
+	public function store(TopicRequest $request, Topic $topic)
 	{
-		$topic = Topic::create($request->all());
-		return redirect()->route('topics.show', $topic->id)->with('message', '创建成功');
+        $topic->fill($request->all());
+        $topic->user_id = Auth::id();
+        $topic->save();
+		return redirect()->route('topics.show', $topic->id)->with('message', '帖子创建成功');
 	}
 
+    /**
+     * 修改话题页
+     * @param Topic $topic
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
 	public function edit(Topic $topic)
 	{
         $this->authorize('update', $topic);
 		return view('topics.create_and_edit', compact('topic'));
 	}
 
+    /**
+     * 修改话题
+     * @param TopicRequest $request
+     * @param Topic $topic
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
 	public function update(TopicRequest $request, Topic $topic)
 	{
 		$this->authorize('update', $topic);
@@ -50,6 +84,12 @@ class TopicsController extends Controller
 		return redirect()->route('topics.show', $topic->id)->with('message', '更新成功');
 	}
 
+    /**
+     * 删除话题
+     * @param Topic $topic
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
 	public function destroy(Topic $topic)
 	{
 		$this->authorize('destroy', $topic);
--- app/helpers.php
+++ app/helpers.php
@@ -10,3 +10,10 @@ function category_nav_active($category_id)
 {
     return active_class( (if_route('categories.show')) && if_route_param('category',$category_id) );
 }
+
+//制作文章摘录
+function make_excerpt($value, $length = 200)
+{
+    $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
+    return str_limit($excerpt, $length);
+}
