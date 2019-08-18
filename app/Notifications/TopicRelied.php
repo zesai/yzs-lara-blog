4f373afd32aaf2c6b641f2f96cc8169b029ae92e
vagrant <vagrant@homestead>
2019-08-18 13:34:36 +0000

    消息通知有新的回复

---
+++ app/Notifications/TopicRelied.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Reply;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class TopicRelied extends Notification
+{
+    use Queueable;
+
+    public $reply;
+
+    /**
+     * 注入回复实体，方便 toDatabase 方法中使用
+     * @param Reply $reply
+     */
+    public function __construct(Reply $reply)
+    {
+        $this->reply = $reply;
+    }
+
+    /**
+     * 开启数据库通知频道
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['database'];
+    }
+
+    /**
+     * @param $notifiable
+     * @return array
+     */
+    public function toDatabase($notifiable)
+    {
+        $topic = $this->reply->topic;
+        $link = $topic->link(['#reply' . $this->reply->id]);
+
+        //存入数据库中 data 的数据
+        return [
+            'reply_id' => $this->reply->id,
+            'reply_content' => $this->reply->content,
+            'user_id' => $this->reply->user->id,
+            'user_name' => $this->reply->user->name,
+            'user_avatar' => $this->reply->user->avatar,
+            'topic_link' => $link,
+            'topic_id' => $topic->id,
+            'topic_title' => $topic->title,
+        ];
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Reply;
+use App\Notifications\TopicRelied;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -11,12 +12,17 @@ class ReplyObserver
 {
     public function creating(Reply $reply)
     {
+        //预防 XSS 漏洞
         $reply->content = clean($reply->content, 'user_topic_body');
     }
 
     public function created(Reply $reply)
     {
+        //发表回复后更新话题总回复数
         $reply->topic->reply_count = $reply->topic->replies->count();
         $reply->topic->save();
+
+        // 通知话题作者有新的评论
+        $reply->topic->user->notify(new TopicRelied($reply));
     }
 }
