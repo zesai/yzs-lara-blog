10b35aa99c119ce19b54a2fb715da3bee7112214
vagrant <vagrant@homestead>
2019-09-05 14:06:30 +0000

    修改文章列表图片样式

--- resources/views/topics/_topic_list.blade.php
+++ resources/views/topics/_topic_list.blade.php
@@ -3,26 +3,31 @@
         @foreach ($topics as $topic)
             <li class="media">
 {{--                <div class="mb-4" style="max-width: 540px;">--}}
-                <div class="mb-4" style="width: 100%;height: 178.33px;">
-                    <div class="row no-gutters" style="height: 100%">
+                <div class="mb-4" style="width: 100%;">
+                    <div class="row no-gutters">
                         <div class="col-md-4">
 {{--                            <img src="..." class="card-img" alt="...">--}}
-                            <a href="{{ route('users.show', [$topic->user_id]) }}">
+                            <a href="{{ $topic->link() }}">
 {{--                                <img class="media-object img-thumbnail mr-3" style="width: 52px; height: 52px;" src="{{ $topic->user->avatar }}" title="{{ $topic->user->name }}">--}}
-                                <img class="media-object img-thumbnail mr-3" style="width:100%;height:100%;" src="{{ $topic->user->avatar }}" title="{{ $topic->user->name }}">
+                                <img class="media-object img-thumbnail mr-3" style="width:100%;height:178.33px;" src="{{ $topic->user->avatar }}" title="{{ $topic->user->name }}">
                             </a>
                         </div>
                         <div class="col-md-8">
-                            <div class="card-body" style="height: 100%;">
-                                <h5 class="card-title" style="height: 25%">
-                                    <a href="{{ $topic->link() }}" title="{{ $topic->title }}">
-                                        {{ $topic->title }}
-                                    </a>
-                                </h5>
-                                <p class="card-text"  style="height: 50%">
-                                    {{ $topic->excerpt }}
-                                </p>
-                                <p class="card-text" style="height: 25%">
+                            <div class="card-body" style="height: 178.33px;">
+                                <div class="topic-list-right" style="height: 30%;">
+                                    <h5 class="card-title">
+                                        <a href="{{ $topic->link() }}" title="{{ $topic->title }}">
+                                            {{ $topic->title }}
+                                        </a>
+                                    </h5>
+                                </div>
+                                <div class="topic-list-right" style="height: 60%;">
+                                    <p class="card-text">
+                                        {{ $topic->excerpt }}
+                                    </p>
+                                </div>
+
+                                <p class="card-text" style="height: 10%">
                                     <small class="media-body meta text-secondary">
 
                                         <a class="text-secondary" href="{{ route('categories.show', $topic->category_id) }}" title="{{ $topic->category->name }}">
